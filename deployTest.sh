e8b9fb0ddb47c1fdb83132a3185a2af29cae0ee2
zhoudzj@163.com <Mark@zhoudideMacBook-Pro-2.local>
2020-10-13 21:45:59 +0800

    init

---
+++ deployTest.sh
@@ -0,0 +1,8 @@
+server="root@39.100.127.116"
+
+path="/var/www/html/mingou_back"
+
+ssh ${server} "cd ${path} && rm -rf *"
+scp -r dist/* ${server}:${path}
+
+echo "\033[32;5m==================已上传至服务👌==================\033[0m" 
